0852670a964a2aa0c6334b6d2d16c7fcd56cdf0e
florianGierlichs <gierlichs@gmx.de>
2020-03-02 17:40:17 +0100

    Add search component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./index.scss";
 
 import { title } from "./components/title";
+import { search } from "./components/search";
 
 document.body.appendChild(title("Poke"));
+document.body.appendChild(search());
