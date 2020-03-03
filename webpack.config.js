e6cf00ea8a6303204e75b21c083dcc8d3a7354e0
florianGierlichs <gierlichs@gmx.de>
2020-03-03 10:13:20 +0100

    Add dart-sass, delete node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,13 @@ module.exports = {
           // Translates CSS into CommonJS
           "css-loader",
           // Compiles Sass to CSS
-          "sass-loader"
+          {
+            loader: "sass-loader",
+            options: {
+              // Prefer `dart-sass`
+              implementation: require("sass")
+            }
+          }
         ]
       }
     ]
