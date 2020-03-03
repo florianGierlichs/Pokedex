55f488a121d810086bf0dc1cc4d150e5dcf359a8
florianGierlichs <gierlichs@gmx.de>
2020-03-03 17:16:48 +0100

    Add logo

--- src/components/title.js
+++ src/components/title.js
@@ -1,11 +1,17 @@
 import { createElement } from "../lib/dom";
 import "./title.scss";
+import logo from "../assets/pokeball.svg";
 
 export function title(text) {
+  const image = createElement("img", {
+    className: "logo",
+    src: logo
+  });
+
   const element = createElement("h1", {
     innerText: text,
     className: "title"
   });
 
-  return element;
+  return [element, image];
 }
